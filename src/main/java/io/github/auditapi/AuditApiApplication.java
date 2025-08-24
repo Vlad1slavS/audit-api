67d5a8be0458e1ef1ca7920f266f1d184c5f24f8
vladstepanov <xstep759@gmail.com>
2025-08-24 14:38:17 +0900

    init commit

---
+++ src/main/java/io/github/auditapi/AuditApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.auditapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditApiApplication.class, args);
+    }
+
+}
